1a05fed04b493d0d677a695d60f1ff19de1476a0
Ruben van der Zaag <ruben.vanderzaag@nelen-schuurmans.nl>
2022-10-06 08:49:27 +0200

    06102022 upload: inc. kwetsbaarheden afleiden

--- 01 - Schematisaties aanmaken/login.py
+++ 01 - Schematisaties aanmaken/login.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import configparser
 import os
 
@@ -12,18 +11,3 @@ def get_login_details(section: str = '3Di', option: str = None):
     config = configparser.RawConfigParser()
     config.read(login_details_ini_fn)
     return config.get(section=section, option=option)
-=======
-import configparser
-import os
-
-
-def get_login_details(section: str = '3Di', option: str = None):
-    """
-    Read data from login_details.ini
-    """
-    path = os.path.abspath(os.path.dirname(__file__))
-    login_details_ini_fn = os.path.join(path, 'login_details.ini')
-    config = configparser.RawConfigParser()
-    config.read(login_details_ini_fn)
-    return config.get(section=section, option=option)
->>>>>>> Stashed changes
